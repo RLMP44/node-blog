96e66a83caead76e05fcff58d1cd667974451143
Rachael Momber <rlmomber@gmail.com>
2025-04-14 20:26:55 -0400

    add: navbar links, main pages and routes, post capabilities

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(express.json());
+
+var posts = {}
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index.ejs', { data: posts });
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  posts[req.body.title] = req.body.content;
+  res.render('index.ejs', { data: posts });
+});
+
+app.get('/bio', (req, res) => {
+  res.render('bio.ejs');
+})
+
+app.get('/works', (req, res) => {
+  res.render('works.ejs');
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs');
 })
 
 app.listen(port, () => {
