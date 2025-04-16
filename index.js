cf71faf81ba40c80d477022fde9380c71219ece6
Rachael Momber <rlmomber@gmail.com>
2025-04-15 21:30:47 -0400

    feat: add edit capabilities for posts

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import methodOverride from "method-override";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.use(express.static('public'));
 app.use(express.json());
 
-var posts = {}
+var posts = []
 
 app.get('/', (req, res) => {
   res.render('index.ejs', { data: posts });
 })
 
 app.post('/', (req, res) => {
-  console.log(req.body);
-  posts[req.body.title] = req.body.content;
+  posts.push({ title: req.body.title, content: req.body.content});
   res.render('index.ejs', { data: posts });
 });
+
+app.get('/edit-post', (req, res) => {
+  res.render('edit-post.ejs', { data: posts[req.query.oldPostID], id: req.query.oldPostID });
+});
+
+app.post('/edit-post', (req, res) => {
+  const input = { title: req.body.newTitle, content: req.body.newContent }
+  posts[req.body.oldPostID] = input;
+  res.render('index.ejs', { data: posts });
+})
 
 app.get('/bio', (req, res) => {
   res.render('bio.ejs');
